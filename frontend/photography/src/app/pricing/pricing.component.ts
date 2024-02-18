3356db7e8988e1ffd16c25e5a7fddb509b6d757b
Kraken <h3nry.pas@gmail.com>
2024-02-18 15:20:04 -0500

    Added some stuff to pricing

--- frontend/photography/src/app/pricing/pricing.component.ts
+++ frontend/photography/src/app/pricing/pricing.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-pricing',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './pricing.component.html',
   styleUrl: './pricing.component.css'
 })
