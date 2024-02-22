f72c79622b67d7817a42dcc129c4a994657656ef
James <jamesgwhit@gmail.com>
2024-02-22 01:13:05 -0500

    fix

--- frontend/photography/src/app/app.component.ts
+++ frontend/photography/src/app/app.component.ts
@@ -4,14 +4,14 @@ import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { TermsComponent } from './terms/terms.component';
 import { PricingComponent } from './pricing/pricing.component';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgFor } from '@angular/common';
 import { WorksComponent } from './works/works.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, NavbarComponent, TermsComponent, PricingComponent, CommonModule, WorksComponent],
+  imports: [RouterOutlet, HomeComponent, NavbarComponent, TermsComponent, PricingComponent, CommonModule, WorksComponent, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- frontend/photography/src/app/pricing/pricing.component.ts
+++ frontend/photography/src/app/pricing/pricing.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-pricing',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, NgFor],
   templateUrl: './pricing.component.html',
   styleUrl: './pricing.component.css'
 })
 export class PricingComponent {
+  constructor(){
+    this.addData();
+  }
+
+  public pricingData: [{[key:string]:string}] = [{}];
+
+  private addData(){
+    this.pricingData.pop()
+    this.pricingData.push({"title":"Mini Session", "description": "Ideal for quick updates or headshots. Includes 30 minutes - 1 hour of shooting time. Minimum of 10 high resolution edited photos.", "image": "funny-cat.jpg", "price": "150"});
+    this.pricingData.push({"title": "Standard Session", "description": "Perfect for families, couples, or individuals. Includes 1 -> 2 hours of shooting time", "image": "funny-cat.jpg","price": "250"});
+    this.pricingData.push({"title": "Extended Session", "description": "Great for larger groups or special occasions. Includes 2 -> 3 hours of shooting time", "image": "funny-cat.jpg","price": "400"});
+  }
+
+
+
 
 }
